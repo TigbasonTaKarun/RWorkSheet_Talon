0fbc976bb9879da2cc84032f2084efa2cff06d09
Cedric Mikhail <cedricmikhail.talon@students.isatu.edu.ph>
2023-09-20 04:02:44 +0000

    Rworksheet

---
+++ RWorkSheet_Talon#1.R
@@ -0,0 +1,65 @@
+#1vector
+
+ager<-c(34, 28, 22, 36, 27, 18, 52, 39, 42, 29,
+         35, 31, 27, 22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 42, 53, 41,
+         51, 35, 24, 33, 41)
+
+#data points
+
+  length(age)
+ 
+#reciprocal age
+ 
+   reciprocal_age <- 1/age 
+    reciprocal_age
+    
+
+  #newtAge
+    
+    new_age <- c(age,0,age)
+      new_age
+      
+  #sortAge
+      
+      sort_age <-sort(age)
+        sort_age
+        
+  #mininum/maximum of age
+       
+         min_age <- min(age)
+          min_age
+        
+        max_age <- max(age)
+          max_age
+          
+  #setup a vector named data
+          data <- c(2.4, 2.8, 2.1, 2.5, 2.4, 2.2, 2.5, 2.3,
+          2.5, 2.3, 2.4, 2.7)
+            data
+            
+  #new vector for data where you double every value of the data
+          double_data <- c(2 * data)
+            double_data
+  
+  #generate a sequence 
+          int_1to100 <- seq(1, 100)
+            int_1to100
+            
+            
+            int_20to60 <- seq(20, 60)
+              int_20to60
+              
+            
+            int_20to60 <- mean(seq(20, 60))
+              int_20to60
+              
+              
+            int_51to91 <- sum(seq(51, 91))
+              int_51to91
+              
+              
+            int_1to1000 <- seq(1, 1000)
+              int_1to1000
+          
+      
+    
