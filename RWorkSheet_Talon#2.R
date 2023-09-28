af9090e5bcd9baf494927820bed466160d6d8e15
Cedric Mikhail <cedricmikhail.talon@students.isatu.edu.ph>
2023-09-28 04:03:02 +0000

    pushanakhuyupdate

---
+++ RWorkSheet_Talon#2.R
@@ -0,0 +1,67 @@
+#1. Create a Vector using: operator
+    
+    operatorVector <- seq (-5,5)
+    operatorVector
+  
+    
+    #The output will display the numbers from -5 to 5
+
+    
+    #b.
+    x <- 1:7
+    x
+    
+    
+
+#2. Create a Vector using seq() Function
+    
+    sequenceVector <- seq (1,3, by = 0.2)
+    sequenceVector
+
+#3. A factory has a census of its worker.
+    
+    ageVector <- c (34, 28, 22, 36, 27, 18, 52, 39, 42, 29, 35, 31, 27, 22, 37, 34, 19, 20, 57,
+                    49, 50, 37, 46, 25, 17, 37, 43, 53, 41, 51, 35, 24, 33, 41, 53, 40, 18, 44,
+                    38, 41, 48, 27, 39, 19, 30, 61, 54, 58, 26, 18)
+    
+    ageElem1 <-  ageVector[3]
+    ageElem1
+  
+    ageElem2 <- ageVector[c (2,4)]
+    ageElem2
+    
+    
+    ageElem3 <- ageVector[c(-4,-12)]
+    ageElem3
+    
+    
+  #4. Create a vector xx <- c("first"=3, "second"=0, "third"=9). Then named the vector, names(x)
+    
+    x <- c("first"=3, "second"=0, "third"=9)
+    x
+    
+    x [c("first", "third")]
+  
+#5. Create a sequence x from -3:2.
+    
+    x <-(-3:2)
+    x
+    
+    x[2] <- 0
+    x
+
+    
+#6. 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
